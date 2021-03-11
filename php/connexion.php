850528124d868c6c7c55d41a63aa4e168a7483ce
nicolasna77 <nicolas_abreu@live.fr>
2021-03-11 17:13:46 +0100

    connection.php

--- php/connexion.php
+++ php/connexion.php
@@ -9,12 +9,17 @@ if(isset($_POST["submitFormConnexion"]))
 
            //// verification si l'adresse mail et le mot de passe
 
-           if(empty($_POST["email"]) || empty($_POST["password"]))  
+           if(empty($_POST["email"]) && empty($_POST["password"]))  
            {  
+
+           }elseif (empty($_POST["email"])) {
+                    echo 'pas de mail';
+                 } elseif (empty($_POST["password"])) {
+                    echo 'pas de mot de passe ';
+                
                
               
-           }  
-           else  
+           }else  
            {  
               
                 $query = $db->prepare("SELECT * FROM client  WHERE email = :email");  
