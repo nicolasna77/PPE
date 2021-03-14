57ea103973d9a3cf8ddc3aa72a6bcfbb1564518a
nicolasna77 <nicolas_abreu@live.fr>
2021-03-14 22:43:54 +0100

    mise a jour de la bdd
    changement de nom des tables

--- bdd/ppe.sql
+++ bdd/ppe.sql
@@ -28,21 +28,21 @@ SET time_zone = "+00:00";
 -- Structure de la table `client`
 --
 
-DROP TABLE IF EXISTS `client`;
-CREATE TABLE IF NOT EXISTS `client` (
-  `idClient` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `connexions`;
+CREATE TABLE IF NOT EXISTS `connexions` (
+  `idConnexions` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(30) COLLATE utf8_bin NOT NULL,
   `prenom` varchar(30) COLLATE utf8_bin NOT NULL,
   `email` varchar(60) COLLATE utf8_bin NOT NULL,
   `password` varchar(30) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`idClient`)
+  PRIMARY KEY (`idConnexions`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `client`
 --
 
-INSERT INTO `client` (`idClient`, `nom`, `prenom`, `email`, `password`) VALUES
+INSERT INTO `connexions` (`idConnexions`, `nom`, `prenom`, `email`, `password`) VALUES
 (1, 'abreu', 'nicolas', 'nicolas_abreu@live.fr', '12345'),
 (2, 'mathithayan', 'steven', 'stev940@hotmail.fr', '123456');
 
@@ -52,23 +52,23 @@ INSERT INTO `client` (`idClient`, `nom`, `prenom`, `email`, `password`) VALUES
 -- Structure de la table `vetement`
 --
 
-DROP TABLE IF EXISTS `vetement`;
-CREATE TABLE IF NOT EXISTS `vetement` (
-  `idVetement` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `stock`;
+CREATE TABLE IF NOT EXISTS `stock` (
+  `idStock` int(11) NOT NULL AUTO_INCREMENT,
   `genre` varchar(1) COLLATE utf8_bin NOT NULL,
   `typeVet` varchar(30) COLLATE utf8_bin NOT NULL,
   `taille` varchar(30) COLLATE utf8_bin NOT NULL,
   `couleur` varchar(30) COLLATE utf8_bin NOT NULL,
   `quantite` int(11) NOT NULL,
   `prix` int(11) NOT NULL,
-  PRIMARY KEY (`idVetement`)
+  PRIMARY KEY (`idStock`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `vetement`
 --
 
-INSERT INTO `vetement` (`idVetement`, `genre`, `typeVet`, `taille`, `couleur`, `quantite`, `prix`) VALUES
+INSERT INTO `stock` (`idStock`, `genre`, `typeVet`, `taille`, `couleur`, `quantite`, `prix`) VALUES
 (1, 'M', 'Pantalon', 'Adulte', 'Rouge', 20, 30);
 COMMIT;
 
