8f4dc8e9aa53aca76455f4c68e77f6b12d0ac09f
nicolasna77 <nicolas_abreu@live.fr>
2021-03-15 15:16:21 +0100

    update de la vitrine

---
+++ php/index.php
@@ -0,0 +1,27 @@
+<?php
+
+include ("bdd/connectBdd.php"); 
+
+if(isset($_POST["submitEnvoi"])){
+
+echo'ferfz';    
+ 
+    if ($_POST["checkboxConsultation"]== 'on') {
+
+
+        $query = $db->prepare("SELECT * FROM stock");  
+        $query->execute();
+        
+        $data = $query->fetch();
+
+
+     }
+
+
+
+
+}
+
+
+
+?>
