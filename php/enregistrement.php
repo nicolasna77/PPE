307a28c0163ff5cf4582fe076cb6a5ee2a9368aa
Stev940 <stev940@hotmail.fr>
2021-03-15 18:39:28 +0100

    enregistrement

--- php/enregistrement.php
+++ php/enregistrement.php
@@ -1,28 +1,46 @@
 <?php 
+include ("bdd/connectBdd.php");
 
-echo 'koko';
 
 
 
 if(isset($_POST["submitFormEnregistrement"])) {
-
-
-    if(empty($_POST['nom'])) {
-
-    echo ("Le champ nom est vide");
-
-    }
-    else {
-    echo "Le champ nom est rempli";
-    }
-
-   
-
 }
-
 else {
- echo "Passer par la page enregistrement";
+    echo "Passer par la page enregistrement <br>";
+}       
+
+
+
+
+if(empty($_POST['nom']) xor empty($_POST['pren'])) {
+    echo "Le champ nom ou prenom n'est pas rempli <br>";
 }
+
+if ((empty($_POST['nom'])) and (empty($_POST['pren']))){
+    echo "Plusieurs champs ne sont pas remplis  <br>";
+}
+
+if (empty($_POST['inputPassword'])){
+    echo "Veuillez choisir un mot de passe <br>";
+   
+}
+
+
+if (empty($_POST['inputEmail'])){
+    echo "veuillez entrez un email <br>";
+}
+
+
+
+if ($_POST["inputPassword"]==$_POST["inputPasswordvalid"]){
+    echo "carré";
+}
+
+
+
+
+
 
 
 
